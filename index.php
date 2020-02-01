9cd780e408df04525956aee372c7197d34b0490c
Lior Chamla <lchamla@gmail.com>
2020-02-01 14:20:08 +0100

    Priorité sur les listeners

--- index.php
+++ index.php
@@ -1,34 +1,11 @@
 <?php
 
 /**
- * TROISIEME PARTIE : REFACTORING DU CODE DU CONTROLLER DANS DES LISTENERS
+ * QUATRIEME PARTIE : LES PRIORITES DES LISTENERS
  * ------------
- * Rappelez vous de la première partie, nous avions vu que notre OrderController présentait deux problèmes majeurs :
- * 1) Il n'avait pas une responsabilité unique (enregistrer une commande) mais PLEIN de responsabilités (envoyer des emails, logger, envoyer
- * des SMS)
- * 2) Chaque modification de ces comportements annexes (emails, SMS, logs) et chaque ajout ou suppressin d'un de ces comportements 
- * demandait une modification du Controller lui-même, ce qui brisait le principe Open/Closed
  * 
- * C'est désormais terminé ! Grâce au design pattern EventDispatcher et au composant symfony/event-dispatcher qui l'implémente, on a réglé ce 
- * soucis :
- * 1) Le controller fait uniquement ce qu'il doit faire : enregistrer la commande. 
- * 2) Il prévient le dispatcher des étapes qu'il traverse
- * 3) Le dispatcher fait appel à des codes dédiés aux événements qu'on lui soumet
- * 4) Modifier une tâche se fait dans la classe appropriée :
- * - src/Listener/OrderLoggerListener.php si on veut modifier les logs
- * - src/Listener/OrderMailingListener.php si on veut modifier les emails
- * - src/Listener/OrderSmsListener.php si on veut modifier les SMS
- * 5) Le controller n'a plus besoin de recevoir dans son constructeur tous les services (texter, mailer, etc) qui ne l'intéressent pas
+ * On a réglé beaucoup de problèmes ... MAIS.
  * 
- * 
- * On a donc bien refactorisé notre code proprement afin que chose ait sa place et chaque place sa chose (paranoïa de l'informaticien).
- * 
- * Le code actuel fait exactement la même chose que le code de base, mais il est beaucoup plus évolutif et ordonné !
- * 
- * Tout est bien qui finit bien ! Vraiment ?
- * 
- * PROCHAINE SECTION : L'ORDRE D'APPEL DES LISTENERS
- * ---------------
  * Vous l'avez peut-être remarqué : en réalité notre code actuel ne fait pas exactement ce que faisait le code de base. Voilà ce qu'on 
  * faisait auparavant :
  * 1) Log de la commande en cours
@@ -43,19 +20,32 @@
  * 4) Email au client
  * 
  * Par défaut, les listeners sont appelés dans l'ordre dans lesquels on les a ajouté au dispatcher .. Est-il possible de gérer cet ordre ?
- * Nous le découvrirons dans la section suivante sur les priorités des listeners !
+ * 
+ * DECOUVRONS LES PRIORITES :
+ * -------------
+ * Lorsqu'on ajoute un listener au dispatcher sur un événement donné, on peut aussi spécifier une priorité afin que le dispatcher en tienne
+ * compte lorsque l'événement apparaitra.
+ * 
+ * Le nombre le plus haut (le plus important donc) a la priorité sur le nombre le plus bas. Par défaut, la priorité assigné à chaque
+ * listener est de 0.
+ * 
+ * On va donc donner des priorités à nos Listeners pour qu'ils soient appelés dans l'ordre que l'on veut, du plus grand au plus petit.
+ * 
+ * Et voilà, le soucis est réglé !
+ * 
+ * --------------
+ * Dans la prochaine section, on découvrira la notion de subscribers : vous l'avez remarqué, ici, nous avons des objets qui parfois possèdent
+ * plusieurs méthodes qui vont être attachées au dispatcher. On peut encore refactoriser ces codes en créant des classes de type Subscriber.
+ * 
+ * Elles permettent de rationnaliser la configuration des listeners en un seul endroit : dans la classe elle-même.
  * 
  * ----------------
  * Pour bien comprendre cette section, regardez en particulier les fichiers suivantes :
- * - src/Listener/OrderMailingListener.php : on met en place les e-mails envoyés avant et apres le passage d'une commande
- * - src/Listener/OrderLoggerListener.php : on met en place les logs à écrire pour le passage d'une commande
- * - src/Listener/OrderSmsListener.php : on met en place l'envoi de SMS au client après passage d'une commande
- * - index.php : on créé le $dispatcher, on lui attache les listeners et on le passe au controller
+ * - index.php : on donne des priorités à nos listeners
  */
 
 use App\Controller\OrderController;
 use App\Database;
-use App\Event\OrderEvent;
 use App\Listener\OrderLoggerListener;
 use App\Listener\OrderMailingListener;
 use App\Listener\OrderSmsListener;
@@ -84,14 +74,43 @@ $orderSmsListener = new OrderSmsListener($smsTexter, $logger);
 $orderLoggerListener = new OrderLoggerListener($logger);
 
 // Mise en place des écoutes :
+// Notez que nous donnons un dernier paramètre numérique à la fonction addListener() : c'est la priorité.
+// Rappelez vous que la priorité ira du nombre le plus grand au nombre le plus petit et se calcule PAR EVENEMENT
+// On a donc ici deux listeners sur l'événement order.before_save (le 2 et le 1) puis deux listeners sur l'événement order.after_save (le 2 et
+// le 1)
+
 // 1. L'envoi de mail au stock quand une commande est en cours de création (juste avant insertion en base de données)
-$dispatcher->addListener('order.before_save', [$orderMailingListener, 'onBeforeOrderIsCreated']);
+$dispatcher->addListener('order.before_save', [$orderMailingListener, 'onBeforeOrderIsCreated'], 1);
 // 2. Le log de la commande en cours de création (juste avant insertion en base de données)
-$dispatcher->addListener('order.before_save', [$orderLoggerListener, 'onBeforeOrderIsCreated']);
+$dispatcher->addListener('order.before_save', [$orderLoggerListener, 'onBeforeOrderIsCreated'], 2);
 // 3. L'envoi de SMS au client après enregistrement de la commande (juste après insertion en base)
-$dispatcher->addListener('order.after_save', [$orderSmsListener, 'onAfterOrderIsCreated']);
+$dispatcher->addListener('order.after_save', [$orderSmsListener, 'onAfterOrderIsCreated'], 1);
 // 4. L'envoi de mail au client après enregistrement de la commande (juste après insertion en base)
-$dispatcher->addListener('order.after_save', [$orderMailingListener, 'onAfterOrderIsCreated']);
+$dispatcher->addListener('order.after_save', [$orderMailingListener, 'onAfterOrderIsCreated'], 2);
+
+/**
+ * ON PEUT CONNAITRE LES PRIORITES SI ON LE SOUHAITE :
+ * -----------
+ * Imaginons que vous ayez une dizaine ou une vingtaine de listeners déclarés dans des fichiers différents à divers endroits, ça commencerait
+ * à être complexe de savoir qui a quelle priorité et où placer un nouveau listener dans la hiérarchie des priorités.
+ * 
+ * Pas de panique ! 
+ * 
+ */
+
+// On récupère les listeners sur un événement donné
+$listeners = $dispatcher->getListeners('order.before_save');
+
+var_dump("Priorité des listeners sur l'évenement order.before_save :");
+
+// Pour chaque listener, on demande quelle est sa priorité sur l'événement donné
+foreach ($listeners as $listener) {
+    $priority = $dispatcher->getListenerPriority('order.before_save', $listener);
+    var_dump([
+        'priority' => $priority,
+        'listener' => $listener
+    ]);
+}
 
 
 $controller = new OrderController($database, $dispatcher);
